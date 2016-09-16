6c8b47abc2b17a4b25c88d4be493589674102de9
jreboqui <jreboqui@ucsd.edu>
2016-09-15 21:26:26 -0700

    Add files via upload
    Repeats until user selects the 'Exit' Option implemented by using a do-while loop.

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,3 +1,7 @@
+/* This version, we are modifying the data validation, we check if the value
+/  is an integer that is 1-4, then we repeatedly show the menu until the user
+/  enters 4, which exits the program
+*/
 #include <iostream>
 
 using namespace std;
@@ -5,18 +9,38 @@ using namespace std;
 int main()
 {
     int choice;
+    
+    do{
+        cout << "Serendipity Booksellers\n";
 
-    cout << "Serendipity Booksellers\n";
+        cout << "\tMain Menu\n\n";
 
-    cout << "\tMain Menu\n\n";
+        cout << "1. Cashier Module\n";
+        cout << "2. Inventory Database Module\n";
+        cout << "3. Report Module\n";
+        cout << "4. Exit\n\n";
 
-    cout << "1. Cashier Module\n";
-    cout << "2. Inventory Database Module\n";
-    cout << "3. Report Module\n";
-    cout << "4. Exit\n\n";
+        cout << "Enter Your Choice: ";
+        cin >> choice;
+        /* Data Validation with switch branching(for later use) */
+        if (choice < 1 || choice > 4){
+            cout << "Please enter a valid choice next time!\n";
+        }
 
-    cout << "Enter Your Choice: ";
-    cin >> choice;
-    cout << "You selected " << choice << "\n";
+        else
+        {
+            switch(choice)
+            {
+                case 1: cout << "You selected 1 \n\n";
+                        break;
+                case 2: cout << "You selected 2 \n\n";
+                        break;
+                case 3: cout << "You selected 3 \n\n";
+                        break;
+                case 4: cout << "You selected 4 \n\n";
+                        break;
 
+            }//end of switch
+        } //end of else
+    }while (choice !=4);
 }
--- reports.cpp
+++ reports.cpp
@@ -5,22 +5,49 @@ using namespace std;
 int main()
 {
     int choice;
+    do
+    {
 
-    cout << "Serendipity Booksellers\n";
-    cout << "\tReports\n\n";
+        cout << "Serendipity Booksellers\n";
+        cout << "\tReports\n\n";
+    
+        cout << "1. Inventory Listing\n";
+        cout << "2. Inventory Wholesale Value\n";
+        cout << "3. Inventory Retail Value\n";
+        cout << "4. Listing by Quantity\n";
+        cout << "5. Listing by Cost\n";
+        cout << "6. Listing by Age\n";
+        cout << "7. Return to the Main Menu\n";
+    
+        cout << "Enter Your Choice: ";
+        cin >> choice;
+        /* Data Validation with switch branching(for later use) */
+        if (choice > 7){
+            cout << "Please enter a valid choice next time!\n";
+        }
+    
+        else
+        {
+            switch(choice)
+            {
+                case 1: cout << "You selected 1 \n\n";
+                        break;
+                case 2: cout << "You selected 2 \n\n";
+                        break;
+                case 3: cout << "You selected 3 \n\n";
+                        break;
+                case 4: cout << "You selected 4 \n\n";
+                        break;
+                case 5: cout << "You selected 5 \n\n";
+                        break;
+                case 6: cout << "You selected 6 \n\n";
+                        break;
+                case 7: cout << "You selected 7 \n\n";
+                        break;
 
-    cout << "1. Inventory Listing\n";
-    cout << "2. Inventory Wholesale Value\n";
-    cout << "3. Inventory Retail Value\n";
-    cout << "4. Listing by Quantity\n";
-    cout << "5. Listing by Cost\n";
-    cout << "6. Listing by Age\n";
-    cout << "7. Return to the Main Menu\n";
-
-    cout << "Enter Your Choice: ";
-    cin >> choice;
-    cout << "\nYou selected " << choice << "\n";
-
+            }//end of switch
+        } //end of else
+    } while (choice != 7);
 
 }
 
