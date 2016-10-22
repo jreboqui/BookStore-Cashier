a5d1f53e1672dacdf15ebf5d39b2eb3997ae9965
Junnel Reboquio <jreboqui@ucsd.edu>
2016-10-21 23:30:11 -0700

    Add files via upload

--- reports.cpp
+++ reports.cpp
@@ -189,13 +189,221 @@ void repRetail()
 
 void repQty()
 {
-    cout << "\nYou selected Inventory Quantity.\n";
+    int id[SIZE]; //array to track original subscripts
+    int* idPtr[SIZE]; //pointer to tracking array
+    int* qtyPtr[SIZE]; //array of pointers to qtyOnHand array
+    
+    for (int i = 0; i< SIZE; i++)
+    {
+        id[i] = i;
+        idPtr[i] = &id[i];
+        qtyPtr[i] = &qtyOnHand[i];
+
+    }
+    
+    int startScan;
+    int maxIndex;
+    int* tempId;
+    int* maxValue;
+
+    //selection sort to do qtyOnHand in descending order
+    for (startScan = 0; startScan < SIZE - 1; startScan++)
+    {
+        maxIndex = startScan;
+        maxValue = qtyPtr[startScan];
+        tempId = idPtr[startScan];
+
+        for (int index = startScan + 1; index < SIZE; index++)
+        {
+            if (*(qtyPtr[index]) > *maxValue)
+            {
+                maxValue = qtyPtr[index];
+                tempId = idPtr[index];
+                maxIndex = index;
+            }
+        }
+        qtyPtr[maxIndex] = qtyPtr[startScan];
+        idPtr[maxIndex] = idPtr[startScan];
+        qtyPtr[startScan] = maxValue;
+        idPtr[startScan] = tempId;
+    }
+
+    //Prompt user for date and then print the infosheet
+    string date; //todays date
+    cout << "\n\nEnter Today's date: ";
+    cin >> date;
+    cout << endl;
+
+    cout << "\n\n\t\tSerendipity Booksellers Quantity Report\n";
+    cout << "Date: \t\t" << date;
+    
+    cout << "\n\tTitle\t\t\t ISBN\t\tQuantity\n";
+    cout << "\t___________________________________________________\n\n";
+    
+    for (int j = 0; j < SIZE; j++)
+    {
+        if (isbn[*(idPtr[j])] != "") //not printing empty records
+        {
+            cout << "\n\t" << left << setw(26) << bookTitle[*(idPtr[j])];
+            cout << left  << setw(14) << isbn[*(idPtr[j])];
+            cout << right << setw(6) << *qtyPtr[j] << "\n";
+        }
+    }
+
+    cout << "\n\n_______________________________________________";
+    cout << "\n\nEnd of Quantity Report\n\n\n";
+    
+    //pause report on screen
+    cout << "Press enter to continue";
+    cin.ignore();
+    cin.get();
 }
+
 void repCost()
 {
-    cout << "\nYou selected Inventory Cost.\n";
+    int id[SIZE]; //array to track original subscripts
+    int* idPtr[SIZE]; //pointer to tracking array
+    double* wholePtr[SIZE]; //array of pointers to qtyOnHand array
+    
+    for (int i = 0; i< SIZE; i++)
+    {
+        id[i] = i;
+        idPtr[i] = &id[i];
+        wholePtr[i] = &wholesale[i];
+
+    }
+    
+    int startScan;
+    int maxIndex;
+    int* tempId;
+    double* maxValue;
+
+    //selection sort to do qtyOnHand in descending order
+    for (startScan = 0; startScan < SIZE - 1; startScan++)
+    {
+        maxIndex = startScan;
+        maxValue = wholePtr[startScan];
+        tempId = idPtr[startScan];
+
+        for (int index = startScan + 1; index < SIZE; index++)
+        {
+            if (*(wholePtr[index]) > *maxValue)
+            {
+                maxValue = wholePtr[index];
+                tempId = idPtr[index];
+                maxIndex = index;
+            }
+        }
+        wholePtr[maxIndex] = wholePtr[startScan];
+        idPtr[maxIndex] = idPtr[startScan];
+        wholePtr[startScan] = maxValue;
+        idPtr[startScan] = tempId;
+    }
+    
+    //Prompt user for date and then print the infosheet
+    string date; //todays date
+    cout << "\n\nEnter Today's date: ";
+    cin >> date;
+    cout << endl;
+
+    cout << "\n\n\t\tSerendipity Booksellers Cost  Report\n";
+    cout << "Date: \t\t" << date;
+    
+    cout << "\n\tTitle\t\t\t ISBN\t\tQuantity\tWholesale\n";
+    cout << "\t___________________________________________________\n\n";
+    
+    for (int j = 0; j < SIZE; j++)
+    {
+        if (isbn[*(idPtr[j])] != "") //not printing empty records
+        {
+            cout << "\n\t" << left << setw(26) << bookTitle[*(idPtr[j])];
+            cout << left  << setw(14) << isbn[*(idPtr[j])];
+            cout << fixed << showpoint << right << setprecision(2);
+            cout << right << setw(6) << qtyOnHand[j];
+            cout << setw(6) << "\t$ " << *wholePtr[j] << "\n\n";
+        }
+    }
+
+    cout << "\n\n_______________________________________________";
+    cout << "\n\nEnd of Cost  Report\n\n\n";
+    
+    //pause report on screen
+    cout << "Press enter to continue";
+    cin.ignore();
+    cin.get();
+
 }
 void repAge()
 {
-    cout << "\nYou selected Inventory Listing by Age.\n";
+    int id[SIZE]; //array to track original subscripts
+    int* idPtr[SIZE]; //pointer to tracking array
+    string* datePtr[SIZE]; //array of pointers to date added  array
+    
+    for (int i = 0; i< SIZE; i++)
+    {
+        id[i] = i;
+        idPtr[i] = &id[i];
+        datePtr[i] = &dateAdded[i];
+
+    }
+
+    int startScan;
+    int maxIndex;
+    int* tempId;
+    string* maxValue;
+
+    //selection sort to do qtyOnHand in descending order
+    for (startScan = 0; startScan < SIZE - 1; startScan++)
+    {
+        maxIndex = startScan;
+        maxValue = datePtr[startScan];
+        tempId = idPtr[startScan];
+
+        for (int index = startScan + 1; index < SIZE; index++)
+        {
+            if (*(datePtr[index]) > *maxValue)
+            {
+                maxValue = datePtr[index];
+                tempId = idPtr[index];
+                maxIndex = index;
+            }
+        }
+        datePtr[maxIndex] = datePtr[startScan];
+        idPtr[maxIndex] = idPtr[startScan];
+        datePtr[startScan] = maxValue;
+        idPtr[startScan] = tempId;
+    }
+    
+    //Prompt user for date and then print the infosheet
+    string date; //todays date
+    cout << "\n\nEnter Today's date: ";
+    cin >> date;
+    cout << endl;
+
+    cout << "\n\n\t\tSerendipity Booksellers Age  Report\n";
+    cout << "Date: \t\t" << date;
+    
+    cout << "\n\tTitle\t\t\t ISBN\t\tQuantity\tDateAdded\n";
+    cout << "\t___________________________________________________\n\n";
+    
+    for (int j = 0; j < SIZE; j++)
+    {
+        if (isbn[*(idPtr[j])] != "") //not printing empty records
+        {
+            cout << "\n\t" << left << setw(26) << bookTitle[*(idPtr[j])];
+            cout << left  << setw(14) << isbn[*(idPtr[j])];
+            cout << fixed << showpoint << right << setprecision(2);
+            cout << right << setw(6) << qtyOnHand[j];
+            cout << setw(6) << "\t$ " << dateAdded[*(idPtr[j])] << "\n\n";
+        }
+    }
+
+    cout << "\n\n_______________________________________________";
+    cout << "\n\nEnd of Age  Report\n\n\n";
+    
+    //pause report on screen
+    cout << "Press enter to continue";
+    cin.ignore();
+    cin.get();
+
 }
